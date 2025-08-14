ebcad6d9df350c50e3fb93549833014bb69e62d4
Akankhya-Dash <akankhyapvt@gmail.com>
2025-08-14 10:15:20 +0530

    Updated StudentManagementSystem.java

--- StudentManagementSystem/StudentManagementSystem.java
+++ StudentManagementSystem/StudentManagementSystem.java
@@ -2,32 +2,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Class to represent a Student
-class Student {
-    private String name;
-    private int rollNumber;
-    private String course;
-    private String email;
-
-    public Student(String name, int rollNumber, String course, String email) {
-        this.name = name;
-        this.rollNumber = rollNumber;
-        this.course = course;
-        this.email = email;
-    }
-
-    public int getRollNumber() {
-        return rollNumber;
-    }
-
-    public void displayStudent() {
-        System.out.println("Name: " + name);
-        System.out.println("Roll Number: " + rollNumber);
-        System.out.println("Course: " + course);
-        System.out.println("Email: " + email);
-        System.out.println("--------------------------------");
-    }
-}
 
 // Class to manage the list of students
 class StudentManager {
