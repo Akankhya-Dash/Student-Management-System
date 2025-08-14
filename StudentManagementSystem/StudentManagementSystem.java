d97a64992fedfab6832243b169f8db24752e6675
Akankhya-Dash <akankhyapvt@gmail.com>
2025-08-14 10:16:50 +0530

    Updated StudentManagementSystem.java

--- StudentManagementSystem/StudentManagementSystem.java
+++ StudentManagementSystem/StudentManagementSystem.java
@@ -3,41 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-// Class to manage the list of students
-class StudentManager {
-    private ArrayList<Student> students = new ArrayList<>();
 
-    public void addStudent(String name, int rollNumber, String course, String email) {
-        students.add(new Student(name, rollNumber, course, email));
-        System.out.println("Student added successfully.");
-    }
-
-    public void removeStudent(int rollNumber) {
-        boolean removed = false;
-        for (int i = 0; i < students.size(); i++) {
-            if (students.get(i).getRollNumber() == rollNumber) {
-                students.remove(i);
-                System.out.println("Student removed successfully.");
-                removed = true;
-                break;
-            }
-        }
-        if (!removed) {
-            System.out.println("Student not found.");
-        }
-    }
-
-    public void displayStudents() {
-        if (students.size() == 0) {
-            System.out.println("No students to display.");
-        } else {
-            System.out.println("List of Students:");
-            for (Student s : students) {
-                s.displayStudent();
-            }
-        }
-    }
-}
 
 // Main class
 public class StudentManagementSystem {
